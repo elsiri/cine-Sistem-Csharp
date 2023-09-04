d1664b3c62d983ea2397ec3a57cbebde43102d80
kevincastrillon70 <30646221+elsiri@users.noreply.github.com>
2023-09-04 12:34:15 -0500

    Subida de proyecto

---
+++ cineSystem/cineSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cineSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ cineSystem/cineSystem/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cineSystem.Models;
+
+public partial class Usuario
+{
+    public int IdUser { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+}
